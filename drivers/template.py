3b2546ae63bd358c296985a699312e4279d32f27
Naza <uzoukwuc@tcd.ie>
2024-02-01 11:33:28 +0000

    Update template

--- drivers/template.py
+++ drivers/template.py
@@ -1,5 +1,18 @@
 class Driver:
-    def process_lidar(self, ranges):
+    def process_lidar(self, ranges, state):
+        """
+        Produces controls for the car based on sensor input
+
+        Args:
+        
+            ranges (array): the distances of the LiDAR measurements
+            from the bottom of the car counterclockwise around to the
+            front of the car.
+
+            state (VehicleStateSnapshot): metadata about the current state
+            of the vehicle in the world, such as speed and orientation.
+        
+        """
         speed = 0
         steering_angle = 0
         return speed, steering_angle
